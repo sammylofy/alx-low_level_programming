01284f525bb14a681820c27e9d9a91a91391dc55
Samuel OLOFU <coolsamuel71@gmail.com>
2023-06-15 22:17:48 -0700

    updating

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,8 +9,8 @@ int main(void)
 printf("Size of a char: %d byte(s)\n", sizeof(char));
 printf("Size of an int: %lu byte(s)\n", (unsigned long) sizeof(int));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long) sizeof(long));
-printf("Size of a long long int: %lu byte(s)\n");
-printf((unsigned long) sizeof(long long));
+printf("Size of a long long int:");
+printf(" %lu byte(s)\n", (unsigned long) sizeof(long long));
 printf("Size of a float: %lu byte(s)\n", (unsigned long) sizeof(float));
 return (0);
 }
