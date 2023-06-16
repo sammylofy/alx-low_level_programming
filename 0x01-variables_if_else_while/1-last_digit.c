014d634461c6d90fde74be15f6fd93d16d077d55
Samuel OLOFU <coolsamuel71@gmail.com>
2023-06-16 14:18:28 -0700

    uploading code to get last digit from randomly generated numbers

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,36 @@
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *  main entry
+ *
+ *  return 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+	int last_digit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	last_digit = n % 10;
+
+	/* your code goes there */
+	printf("Last digit of %d is %d and ", n, last_digit);
+	if ((last_digit != 0) && (last_digit >= 6))
+	{
+		printf("is greater greater than 5\n");
+	}
+	else if (last_digit < 6 && (last_digit != 0))
+	{
+		printf("is less than 6 and not 0\n");
+	}
+	else
+	{
+		printf("is 0\n");
+	}
+	return (0);
+}
