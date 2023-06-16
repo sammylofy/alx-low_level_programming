5e7416e860ace6858738da64671702c4154f14ea
Samuel OLOFU <coolsamuel71@gmail.com>
2023-06-15 21:44:15 -0700

    adding the files

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,10 @@
  * */
 int main(void)
 {
-printf("Size of a char: %d\n", sizeof(char));
-printf("Size of an int: %lu\n", (unsigned long) sizeof(int));
-printf("Size of a long int: %lu\n", (unsigned long) sizeof(long));
-printf("Size of a long long int: %lu\n", (unsigned long) sizeof(long long));
-printf("Size of a float: %lu\n", (unsigned long) sizeof(float));
+printf("Size of a char: %d byte(s)\n", sizeof(char));
+printf("Size of an int: %lu byte(s)\n", (unsigned long) sizeof(int));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long) sizeof(long));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long) sizeof(long long));
+printf("Size of a float: %lu byte(s)\n", (unsigned long) sizeof(float));
 return (0);
 }
