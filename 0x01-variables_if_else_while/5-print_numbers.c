28624b128cee38b4a7714dfcdd130fb652fdee42
Samuel OLOFU <coolsamuel71@gmail.com>
2023-06-17 02:10:23 -0700

    code to print 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Code snippet to print 0-9
+ *
+ * Return: 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+		printf("%d", digit);
+	printf("\n");
+	return (0);
+}
