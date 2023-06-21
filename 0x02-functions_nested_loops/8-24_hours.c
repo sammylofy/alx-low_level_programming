4a853c5c27cd1fd5ad6076c7c940a5d98bba5f38
Samuel OLOFU <coolsamuel71@gmail.com>
2023-06-21 06:31:12 -0700

    pushing the jack bauer code for 00:00 - 23:59

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Prints the hh : 00
+ *
+ * Description: I used loop in printing them
+ *
+ * Return: returns nothing (void)
+ */
+
+void jack_bauer(void)
+{
+	int hh, mm;
+
+	for (hh = 0; hh < 24; hh++)
+	{
+		for (mm = 0; mm < 60; mm++)
+		{
+			if (hh < 10)
+			{
+				_putchar('0' + hh / 10);
+				_putchar('0' + hh % 10);
+			} else
+			{
+				_putchar('0' + hh / 10);
+				_putchar('0' + hh % 10);
+			}
+			_putchar(' ');
+			_putchar(':');
+			_putchar(' ');
+			if (mm < 10)
+			{
+				_putchar('0' + mm / 10);
+				_putchar('0' + mm % 10);
+			} else
+			{
+				_putchar('0' + mm / 10);
+				_putchar('0' + mm % 10);
+			}
+			_putchar('\n');
+		}
+	}
+}
