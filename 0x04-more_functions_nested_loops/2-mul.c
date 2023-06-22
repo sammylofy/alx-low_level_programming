32b8eab2c0d227e31bda108641c4d840cbcd0796
Samuel OLOFU <coolsamuel71@gmail.com>
2023-06-22 01:56:37 -0700

    pushing the multiplication function online

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiply two numbers of type int
+ * @a: integer value
+ * @b: integer type
+ *
+ * Return: returns the value after mul
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
