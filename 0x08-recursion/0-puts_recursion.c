9ad0804b12b01c02e67746670af5cb5cd75b0c55
Samuel OLOFU <coolsamuel71@gmail.com>
2023-07-05 06:07:27 -0700

    recursive files

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * _puts_recursion - A function to recursively print string
+ * @s: Character array of String
+ * Return: Nothing
+ */
+void _puts_recursion(char *s)
+{
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,2 @@
+Recursion is introduced here. and Recursion is the functional call to handle
+task(s) repitively until desired outcome is achieved instead of the usual loop.
