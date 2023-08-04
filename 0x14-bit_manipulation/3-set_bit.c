d2c961908dfbef422af77af8dc95b10b003f22dc
Samuel OLOFU <coolsamuel71@gmail.com>
2023-08-03 17:28:01 -0700

    uploading the set bit function

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * set_bit - set the bit into 1
+ * @n: the number to be changed
+ * @index: the index to effect the change
+ *
+ * Desription: convert the value
+ * at an index to 1
+ *
+ * Return: the bit is changed to 1
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+
+	unsigned long int mask;
+
+	mask = 1UL << index;
+	*n |= mask;
+
+	return (1);
+}
