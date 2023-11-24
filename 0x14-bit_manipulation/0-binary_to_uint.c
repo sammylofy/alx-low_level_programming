dfa9a68aa4c8cb8f8abc5b46a5477c279d83adec
sammylofy <coolsamuel71@gmail.com>
2023-11-24 03:44:28 -0800

    updated the code to meet Betty

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,34 +1,46 @@
 #include "main.h"
 #include <stdio.h>
 
-/**
- * binary_to_unit - 
- */
-
 unsigned int binaryToUnitHelper(int base, int power);
+
+/**
+ * binary_to_uint - Converts a binary number to an unsigned int.
+ * @b: A pointer to a string of 0 and 1 characters.
+ *
+ * Return: The converted number, or 0 if there's an error.
+ */
 
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int result, length;
 
 	if (b == NULL)
-		return 0;
+		return (0);
 
 	result = 0;
 	length = (unsigned int) strlen(b) - 1;
-	while (*b != '\0') {
+	while (*b != '\0')
+	{
 		if (*b != '0' && *b != '1')
-			return 0;
-		result += (*b - '0')* binaryToUnitHelper(2,length);
+			return (0);
+		result += (*b - '0') * binaryToUnitHelper(2, length);
 		length--;
 		b++;
 	}
-	return result;
+	return (result);
 }
+
+/**
+ * binaryToUnitHelper - Helper function to calculate the power.
+ * @base: The base value to be raised to a power.
+ * @power: The power to which the base is raised.
+ *
+ * Return: The result of raising the base to the power.
+ */
 
 unsigned int binaryToUnitHelper(int base, int power)
 {
-	if(power == 0)
+	if (power == 0)
 		return (1);
 	return (base * binaryToUnitHelper(base, power - 1));
 }
